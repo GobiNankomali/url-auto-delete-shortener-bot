864c8515c14bd8df7ed648c73be3671cde32d2d3
Nankomali <98003797+Nankomali@users.noreply.github.com>
2022-11-16 13:30:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,7 +33,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Nankomali2:gobi474709@cluster0.gwoikam.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Nankomali2")
+DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
