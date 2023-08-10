a3529dbf4583af557f105539dcceef1793477b09
GobiNankomali <119668092+GobiNankomali@users.noreply.github.com>
2023-08-10 14:48:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,8 +68,8 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
     
       # URL Shortener #
 
-URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', ')
-URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', ')
+URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE','dalink.in')
+URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API','7882b53fa93b2906f749831d4039d146cac415a0')
 
      # Auto Delete For Group Message (Self Delete) #
 SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 300))
